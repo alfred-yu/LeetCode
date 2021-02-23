a6576954b960bd42ab738abf1e6634db81ef73a8
Yu Shaojian <shaojian.yu@hotmail.com>
2021-02-23 13:47:02 +0800

    Add leetcode-offer-47

---
+++ leetcode-offer-47/solution_dp.cpp
@@ -0,0 +1,27 @@
+class Solution {
+	public:
+		int maxValue(vector<vector<int>>& grid) {
+			if (grid.size() <= 0 or grid[0].size() <= 0)
+				return 0;
+
+			for (int i = 1; i < grid[0].size(); i++)
+			{
+				grid[0][i] += grid[0][i-1];
+			}
+
+			for (int i = 1; i < grid.size(); i++)
+			{
+				grid[i][0] += grid[i - 1][0];
+			}
+
+			for (int i = 1; i < grid.size(); i++)
+			{
+				for (int j = 1; j < grid[0].size(); j++)
+				{
+					grid[i][j] += max(grid[i - 1][j], grid[i][j - 1]);
+				}
+			}
+
+			return grid[grid.size() - 1][grid[0].size() - 1];
+		}
+};
